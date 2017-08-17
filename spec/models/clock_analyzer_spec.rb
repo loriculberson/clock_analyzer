5d1084b484cea9bdb891ce6c2a6d3713894da90d
Lori Culberson <bigskybandit@sbcglobal.net>
2017-08-16 19:54:32 -0500

    Update specs with contexts

--- spec/models/clock_analyzer_spec.rb
+++ spec/models/clock_analyzer_spec.rb
@@ -1,62 +1,66 @@
 require 'rails_helper'
 
 RSpec.describe ClockAnalyzer do 
-  context "#compute_difference" do 
-    it "returns 0 angle difference between minute and hour hand" do 
+    describe "#compute_difference" do 
+    context "clock hands are in quadrants I and II" do 
+       it "returns 172.5 angle difference between minute and hour hand" do 
+        time = "9:15"
+        clock_analyzer = ClockAnalyzer.new(time)
 
-      time = "12:00"
-      clock_analyzer = ClockAnalyzer.new(time)
+        expect(clock_analyzer.compute_difference).to eq(172.5)
+      end
+     
+      it "returns 115 angle difference between minute and hour hand" do 
+        time = "10:10"
+        clock_analyzer = ClockAnalyzer.new(time)
 
-      expect(clock_analyzer.compute_difference).to eq(0)
+        expect(clock_analyzer.compute_difference).to eq(115)
+      end
     end
 
-    it "returns 105 angle difference between minute and hour hand" do 
+    context "clock hands are in both in quadrant II" do 
+      it "returns 82.5 angle difference between minute and hour hand" do 
+        time = "11:45"
+        clock_analyzer = ClockAnalyzer.new(time)
 
-      time = "2:30"
-      clock_analyzer = ClockAnalyzer.new(time)
-
-      expect(clock_analyzer.compute_difference).to eq(105)
+        expect(clock_analyzer.compute_difference).to eq(82.5)
+      end
     end
 
-    it "returns 82.5 angle difference between minute and hour hand" do 
+    context "clock hands are in parallel to Y axis" do 
+      it "returns 0 angle difference between minute and hour hand" do 
+        time = "12:00"
+        clock_analyzer = ClockAnalyzer.new(time)
 
-      time = "11:45"
-      clock_analyzer = ClockAnalyzer.new(time)
-
-      expect(clock_analyzer.compute_difference).to eq(82.5)
+        expect(clock_analyzer.compute_difference).to eq(0)
+      end
     end
 
-    it "returns 7.5 angle difference between minute and hour hand" do 
+    context "clock hands are in quadrants I and IV" do 
+      it "returns 105 angle difference between minute and hour hand" do 
+        time = "2:30"
+        clock_analyzer = ClockAnalyzer.new(time)
 
-      time = "3:15"
-      clock_analyzer = ClockAnalyzer.new(time)
-
-      expect(clock_analyzer.compute_difference).to eq(7.5)
+        expect(clock_analyzer.compute_difference).to eq(105)
+      end
     end
 
-    it "returns 40 angle difference between minute and hour hand" do 
+    context "clock hands are in quadrant III and the X axis" do 
+      it "returns 7.5 angle difference between minute and hour hand" do 
+        time = "3:15"
+        clock_analyzer = ClockAnalyzer.new(time)
 
-      time = "5:20"
-      clock_analyzer = ClockAnalyzer.new(time)
-
-      expect(clock_analyzer.compute_difference).to eq(40)
+        expect(clock_analyzer.compute_difference).to eq(7.5)
+      end
     end
 
-    it "returns 115 angle difference between minute and hour hand" do 
+    context "clock hands are both in quadrant IV" do 
+      it "returns 40 angle difference between minute and hour hand" do 
+        time = "5:20"
+        clock_analyzer = ClockAnalyzer.new(time)
 
-      time = "10:10"
-      clock_analyzer = ClockAnalyzer.new(time)
-
-      expect(clock_analyzer.compute_difference).to eq(115)
+        expect(clock_analyzer.compute_difference).to eq(40)
+      end
     end
-
-    it "returns 172.5 angle difference between minute and hour hand" do 
-
-      time = "9:15"
-      clock_analyzer = ClockAnalyzer.new(time)
-
-      expect(clock_analyzer.compute_difference).to eq(172.5)
-    end
-
   end
 end
