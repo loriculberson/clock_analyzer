1214ed7c2f86565dc988a841569cfa217c3a510d
Lori Culberson <bigskybandit@sbcglobal.net>
2017-08-17 00:49:45 -0500

    Time and angle now appear when user clicks submit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,10 @@
 class HomeController < ApplicationController
 
   def index
+    if params[:time].present?
+      @time = params[:time]
+      clock_analyzer = ClockAnalyzer.new(@time)
+      @angle = clock_analyzer.compute_difference
+    end
   end
 end
