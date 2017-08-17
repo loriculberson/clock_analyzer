9c5171d4bf8954072f5c9ab373be8096abe70f19
Lori Culberson <bigskybandit@sbcglobal.net>
2017-08-16 19:40:14 -0500

    Angle is calculated when time is input

--- app/models/clock_analyzer.rb
+++ app/models/clock_analyzer.rb
@@ -6,7 +6,7 @@ class ClockAnalyzer
   end
 
   def compute_difference
-    if calc_degrees_minutes <= fractional_hour_in_degrees
+    if calc_diff_in_degrees > 180
       360 - calc_diff_in_degrees
     else
       calc_diff_in_degrees
@@ -14,6 +14,26 @@ class ClockAnalyzer
   end
 
   private
+  def calc_diff_in_degrees
+    (total_hours_in_degrees - total_minutes_in_degrees).abs
+  end
+
+  def total_hours_in_degrees
+    calc_degrees_of_whole_hours + fractional_hour_in_degrees
+  end
+
+  def calc_degrees_of_whole_hours
+      hours * 30
+  end
+
+  def fractional_hour_in_degrees
+    ((total_minutes_in_degrees/360r).to_f * 30) 
+  end
+
+  def total_minutes_in_degrees
+    minutes * 6
+  end
+ 
   def time_parser
     /(\d+)\:(\d+)/.match(time)
   end
@@ -24,21 +44,5 @@ class ClockAnalyzer
 
   def minutes
     time_parser[2].to_i
-  end
-
-  def calc_degrees_hours
-    (hours * 30) + fractional_hour_in_degrees
-  end
-
-  def fractional_hour_in_degrees
-    ((calc_degrees_minutes/360r).to_f * 30)
-  end
-
-  def calc_degrees_minutes
-    minutes * 6
-  end
-
-  def calc_diff_in_degrees
-    (calc_degrees_hours - calc_degrees_minutes).abs
   end
 end
--- spec/models/clock_analyzer_spec.rb
+++ spec/models/clock_analyzer_spec.rb
@@ -25,5 +25,38 @@ RSpec.describe ClockAnalyzer do
 
       expect(clock_analyzer.compute_difference).to eq(82.5)
     end
+
+    it "returns 7.5 angle difference between minute and hour hand" do 
+
+      time = "3:15"
+      clock_analyzer = ClockAnalyzer.new(time)
+
+      expect(clock_analyzer.compute_difference).to eq(7.5)
+    end
+
+    it "returns 40 angle difference between minute and hour hand" do 
+
+      time = "5:20"
+      clock_analyzer = ClockAnalyzer.new(time)
+
+      expect(clock_analyzer.compute_difference).to eq(40)
+    end
+
+    it "returns 115 angle difference between minute and hour hand" do 
+
+      time = "10:10"
+      clock_analyzer = ClockAnalyzer.new(time)
+
+      expect(clock_analyzer.compute_difference).to eq(115)
+    end
+
+    it "returns 172.5 angle difference between minute and hour hand" do 
+
+      time = "9:15"
+      clock_analyzer = ClockAnalyzer.new(time)
+
+      expect(clock_analyzer.compute_difference).to eq(172.5)
+    end
+
   end
 end
